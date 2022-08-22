e4c253bd598dcf517dc32175a5294ebeffd2ec48
Leo-997 <489366635@qq.com>
2022-08-22 15:04:24 +0800

    HD 100

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,17 +6,27 @@ public class PrintAndHide : MonoBehaviour
 {
     public int i = 3;
     public Renderer rend;
+    public int R ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        R = Random.Range(150, 251);// Let the R equal a random int from 150 to 250
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         i++;
         Debug.Log(gameObject.name+":"+i);
-        //GameObject.FindGameObjectWithTag("Red");
+  
+        if( gameObject.tag == "Red" && i == 100) {
+            gameObject.SetActive(false);//Deactivate this gameobject
+        }
+
+        if ( gameObject.tag == "Blue" && i == R) {
+            rend = GetComponent<Renderer>();
+            rend.enabled = false;//Turn off the render
+        }
+
     }
 }
