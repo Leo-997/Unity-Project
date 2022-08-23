4d16910981999e2bbdf76f4d4639418269611642
Leo-997 <489366635@qq.com>
2022-08-23 11:42:25 +0800

    Update LoadAssets.cs

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -9,13 +9,9 @@ public class LoadAssets : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         Application.targetFrameRate = 60;
         Instantiate(redObj, new Vector3(2,0,0), Quaternion.identity);
         Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
-
-  
-
     }
 
     // Update is called once per frame
